6a8c7ec729925c98b671134089ed577423c0f515
Hans Dragnes <hdragnes@gmail.com>
2016-02-25 00:50:24 +0100

    recommit

--- src/application/Grid.java
+++ src/application/Grid.java
@@ -2,14 +2,12 @@ package application;
 
 public class Grid {
 	double[][] grid = new double[][] 	{
-		{0,0,0,0},
+		{0,1,1,0},
 		{0,1,1,0},
 		{1,0,0,1},
-		{0,1,1,0}
 		};
 		
 	public double[][] getGrid(){
 		return grid;
 	}
-
 }
