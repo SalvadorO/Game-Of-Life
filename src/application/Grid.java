9f11749b63fdb4ad317c40d77bca29bd8adc0449
Hans Dragnes <hdragnes@gmail.com>
2016-02-25 00:32:07 +0100

    Updated the Grid

--- src/application/Grid.java
+++ src/application/Grid.java
@@ -2,13 +2,14 @@ package application;
 
 public class Grid {
 	double[][] grid = new double[][] 	{
-		{1,0,0,1},
+		{0,1,1,0},
 		{0,1,1,0},
 		{1,0,0,1},
-		{0,1,1,}
+		{0,1,1,0}
 		};
 		
 	public double[][] getGrid(){
 		return grid;
 	}
+
 }
