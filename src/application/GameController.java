092e884d256cdff32eccd4bac1b24663697115f6
Hans Dragnes <hdragnes@gmail.com>
2016-02-24 22:45:42 +0100

    Re-commit

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -1,19 +1,27 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
-public class GameController {
+public class GameController implements Initializable{
 	
-	protected GOLModel g_model = new GOLModel();
+//	protected GOLModel model = new GOLModel();
 	protected FileManagement filemanager = new FileManagement();
+	@FXML private Canvas img;
+	private GraphicsContext gc;
 	
 	@FXML
-    private GridPane grd_Gameboard;
+    private Canvas canvas;
 
     @FXML
     private MenuItem mnu_FileOpen,  mnu_FileSave;
@@ -25,14 +33,34 @@ public class GameController {
 	@FXML
     private TextArea txtArea	;
     
+	/**
+	 * Method request an array from Grid. Draws this on the canvas
+	 * Code should be moved to model later on. Enabled here for testing purposes
+	 * @param event
+	 * @author hd
+	 */
     @FXML
     void btn_PlayPressed(ActionEvent event) {
-    	g_model.setCell(grd_Gameboard);
-	}
+		int x = 100;
+		int y = 100;
+		int size = 10;
+		
+		Grid g = new Grid();
+		double[][] array = g.getGrid();
+		gc.setFill(Color.BLACK);
+		
+		for (int i = 0;i<array.length;i++){
+			for (int j = 0; j<(array[i].length); j++){
+				if (array[i][j]==1)
+					gc.fillRect((x+size*j), (y+size*i), size, size);
+			}
+		}
+    }
+	
     
     @FXML
     void btn_ResetPressed(ActionEvent event) {
-    	g_model.setCell(grd_Gameboard);
+    	
 	}
     
     @FXML
@@ -50,7 +78,14 @@ public class GameController {
     protected void bnt_CancelPressed(ActionEvent event) {
     	System.exit(0);
     }
-	
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+		gc = img.getGraphicsContext2D();
+		gc.setFill(Color.BLACK);
+		System.out.println("color is set to black");
+		
+	}
+
 }
 
 
--- src/application/Grid.java
+++ src/application/Grid.java
@@ -1,11 +1,12 @@
 package application;
 
 public class Grid {
-	double[][] grid = 
-			new double[][] 	{
-							{1,0},
-							{1,1},
-							};
+	double[][] grid = new double[][] 	{
+		{0,0,0,0},
+		{0,1,1,0},
+		{1,0,0,1},
+		{0,1,1,0}
+		};
 		
 	public double[][] getGrid(){
 		return grid;
