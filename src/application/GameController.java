a4df23b2e6ec17d1d8ec3f6e6f43af0910557fd7
Hans Dragnes <hdragnes@gmail.com>
2016-02-25 14:42:23 +0100

    Created draw method. Moved it from controller to grid

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -48,8 +48,11 @@ public class GameController implements Initializable{
 		Grid g = new Grid();
 		double[][] array = g.getGrid();
 
-		gc.setFill(Color.YELLOW);
+		gc.setFill(Color.BLACK);
 
+		gc.setFill(Color.AQUAMARINE);
+
+		
 		for (int i = 0;i<array.length;i++){
 			for (int j = 0; j<(array[i].length); j++){
 				if (array[i][j]==1)
