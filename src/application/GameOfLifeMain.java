87bb49a84626a25dc44627879bb108f905952f29
Lars Kristian Haga <lars_h96@hotmail.com>
2016-03-09 20:20:28 +0100

    Removed unimportant Imports

--- src/application/GameOfLifeMain.java
+++ src/application/GameOfLifeMain.java
@@ -2,12 +2,8 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
