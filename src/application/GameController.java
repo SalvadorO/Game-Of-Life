82c3c12ce6def647e2e36b7734dfcfba561ff604
Lars Haga <Lars_h96@hotmail.com>
2016-03-16 17:57:34 +0100

    Now it initializes dialog boxes

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -65,62 +65,21 @@ public class GameController implements Initializable{
 		//The lines above, are for testing, to be removed
 			
     }
-	// Code for about dialog
-	@FXML
-	void mnu_AboutDialogPressed(ActionEvent event) {
-		Alert about = new Alert(AlertType.INFORMATION);
-		about.setTitle("About");
-		about.setHeaderText(null);
-		about.setContentText("John Conway's Game Of Life"+"\n\n"+"The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970"+"\n"+"The 'game' is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves or, for advanced players, by creating patterns with particular properties."+"\n\n"+"Rules"+"\n"+"1. Any live cell with fewer than two live neighbours dies, as if caused by under-population."+"\n"+"2. Any live cell with two or three live neighbours lives on to the next generation."+"\n"+"3. Any live cell with more than three live neighbours dies, as if by over-population."+"\n"+"4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction."+"\n\n\n\n\n\n\n\n\n"+"Test");
-		about.setResizable(true);
-		about.initStyle(StageStyle.UNDECORATED);
-		
-		ButtonType buttonTypeCancel = new ButtonType("I understand", ButtonData.CANCEL_CLOSE);
-		about.getButtonTypes().setAll(buttonTypeCancel);
-		
-		about.showAndWait();
-	}
-//	Test txt area lars
-	/*
-	Exception ex = new FileNotFoundException("Could not find file blabla.txt");
-	
-	StringWriter sw = new StringWriter();
-	PrintWriter pw = new PrintWriter(sw);
-	ex.printStackTrace(pw);
-	String exceptionText = sw.toString();
-	
-	Label label = new Label("Test TXT Area");
-	*/
-// Advanced Menu
-	@FXML
-	void rbn_AdvancedMenuPressed(ActionEvent event){
-		Alert advanced = new Alert(AlertType.INFORMATION);
-		advanced.setTitle("Advanced");
-		advanced.setHeaderText(null);
-		advanced.setContentText("Advanced Menu is now enabled! \n\nGood luck!");
-		advanced.setResizable(true);
-		
-		ButtonType buttonTypeCancel = new ButtonType("Close", ButtonData.CANCEL_CLOSE);
-		advanced.getButtonTypes().setAll(buttonTypeCancel);
-		
-		advanced.showAndWait();
-	}
-	
-	
-	@FXML
-	void mnu_StatsMenuPressed(ActionEvent event){
-		Alert stats = new Alert(AlertType.INFORMATION);
-		stats.setTitle("Stats");
-		stats.setHeaderText(null);
-		stats.setResizable(false);
-		stats.setContentText("Here will stats about the game be");
-		stats.initStyle(StageStyle.UNDECORATED);
-		
-		ButtonType buttonTypeCancel = new ButtonType("Close", ButtonData.CANCEL_CLOSE);
-		stats.getButtonTypes().setAll(buttonTypeCancel);
-		
-		stats.showAndWait();
-	}
+	// Help dialog
+			@FXML
+			 void mnu_AboutDialogPressed(ActionEvent event) {
+			 		Dialog.AboutDialogue();
+			 }
+			// Advanced Menu
+			@FXML
+				void rbn_AdvancedPressed(ActionEvent event)	{
+					Dialog.AdvancedDialogue();
+			}
+			// Stats Menu
+			@FXML
+				void mnu_StatsMenuPressed(ActionEvent event)	{
+					Dialog.StatsDialogue();
+			}
 
 	/**
 	 * NOTE: use txtArea for testing output
