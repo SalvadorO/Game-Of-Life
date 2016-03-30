12994700c5c5c279816374b6ec26a94fca4b54ab
Lars Haga <Lars_h96@hotmail.com>
2016-03-30 15:13:52 +0200

    Added the MouseClicker

--- src/application/GameOfLifeMain.java
+++ src/application/GameOfLifeMain.java
@@ -17,7 +17,9 @@ public class GameOfLifeMain extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("GOLView.fxml"));
 			primaryStage.setTitle("Conway's Game of Life");
 			Scene scene = new Scene(root);
-//			scene.getStylesheets().add(getClass().getResource("GameboardCSS.css").toExternalForm());
+			primaryStage.setMinWidth(600);
+			primaryStage.setMinHeight(400);
+			scene.getStylesheets().add(getClass().getResource("GameboardCSS.css").toExternalForm());
 			// Adds a icon to the stage
 			primaryStage.getIcons().add(new Image(this.getClass().getResource("GameOfLife.png").toString()));
 			
@@ -26,10 +28,9 @@ public class GameOfLifeMain extends Application {
 
 	    	scene.setOnMousePressed(new EventHandler<MouseEvent>() {
 	    	        public void handle(MouseEvent event) {
-	    	            System.out.println("mouse click detected! ");
+	    	            System.out.println("You pressed:\nX: "+event.getX()+"\nY: "+event.getY());
 		    	        }
 		    	    });
-			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
