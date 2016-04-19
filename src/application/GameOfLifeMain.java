fd3d5790e22f7ce4cf11ff4f36e3d8a6a9a22e3b
Lars Haga <Lars_h96@hotmail.com>
2016-04-19 10:25:53 +0200

    Public

--- src/application/GameOfLifeMain.java
+++ src/application/GameOfLifeMain.java
@@ -40,7 +40,7 @@ public class GameOfLifeMain extends Application {
 			e.printStackTrace();
 		}
 	}
-	protected static void main(String[] args) {
+	public static void main(String[] args) {
 		launch(args);
 	}	
 }
