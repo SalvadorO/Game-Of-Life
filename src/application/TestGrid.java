3d335b6f013d9ce7242141258afa5927838bf865
Hans Dragnes <hdragnes@gmail.com>
2016-05-04 18:37:54 +0200

    Updated testGrid

--- src/application/TestGrid.java
+++ src/application/TestGrid.java
@@ -6,17 +6,40 @@ import org.junit.Test;
 
 /**
  * TestGrid class
+ * @author hd
  */
 public class TestGrid {
 
+	GameboardCanvas gbc = new GameboardCanvas(10, 10);
+	
 	@Test
-	public void testXXX() {
-		fail("Not yet implemented");
+	public void test_GridGetters() {
+		
+//		Test 1
+		assertTrue(gbc.grid.getColumns() == 10);
+		assertTrue(gbc.grid.getRows() == 10);
+		
+//		Test 2
+		gbc.grid.setGrid(5, 5);
+		assertFalse(gbc.grid.getColumns() == 10);
+		assertFalse(gbc.grid.getRows() == 10);
+		
 	}
-
+	
 	@Test
-	public void testYYY() {
-		fail("Not yet implemented");
+	public void test_setCellStatus() {
+		
+		gbc.grid.setGrid(5, 5);
+		
+//		Test 1
+		gbc.grid.setCellstatus(0, 0, 1);
+		gbc.grid.setCellstatus(4, 4, 1);
+		String expectedresult = "1000000000000000000000001";
+		assertEquals(expectedresult, gbc.grid.toString());
+		
+//		Test 2
+		gbc.grid.setCellstatus(4, 4, 0);
+		assertFalse(expectedresult.equals(gbc.grid.toString()));
 	}
 
 }
