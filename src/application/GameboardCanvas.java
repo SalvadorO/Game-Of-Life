6299e11415b9fcfc925d5fca590ec5b1f69ad91f
Hans Dragnes <hdragnes@gmail.com>
2016-05-04 15:59:40 +0200

    Cosmetics

--- src/application/GameboardCanvas.java
+++ src/application/GameboardCanvas.java
@@ -15,7 +15,7 @@ public class GameboardCanvas {
 //	Grid object, holding the grid array
 	
 	//TODO: should be private, refactor methods calling this to use avoid using static
-	protected Grid grid;
+	private Grid grid;
 	
 	
 	//Constructor, a new gameboard canvas with default gridsize.
